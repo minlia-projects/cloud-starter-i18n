3b5b7a8307b26b55381ec669826817ecca183359
will <will@minlia.com>
2018-07-19 23:01:05 +0800

    Update

--- src/main/java/com/minlia/cloud/i18n/Lang.java
+++ src/main/java/com/minlia/cloud/i18n/Lang.java
@@ -3,6 +3,7 @@ package com.minlia.cloud.i18n;
 import com.minlia.cloud.context.ContextHolder;
 import java.util.Locale;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 
@@ -25,12 +26,16 @@ public class Lang {
   public static String get(String key, Object[] arguments, Locale locale) {
     String result ="{{" + key + "}}";
     try {
-      result= ContextHolder.getContext().getBean(MessageSource.class).getMessage(key, arguments,result, locale);
+      ApplicationContext context=ContextHolder.getContext();
+      if(null!=context) {
+        result = context.getBean(MessageSource.class).getMessage(key, arguments, result, locale);
+      }
     } catch (org.springframework.context.NoSuchMessageException e) {
       log.warn("No translated message found for key: {}", key);
     }
     return result;
   }
+
 
 
 }
